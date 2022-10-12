10336d76c626355db7187d752efe5ada1eab8380
Technotech <78465863+TechnotechYT@users.noreply.github.com>
2022-10-12 19:00:53 +1100

    kinda works

--- js/index.js
+++ js/index.js
@@ -44,30 +44,63 @@ window.onload = function () {
         mouse.x = e.pageX
         mouse.y = e.pageY
 
-        console.log(mouse.x, mouse.y)
         if (down == true) {
-            dragX = e.pageX
+            // console.log(mouse.x, mouse.y)
+            dragX = e.pageX;
             dragY = e.pageY;
+
+            deltaX = dragX - initX, deltaY = dragY - initY;
+            initX = e.pageX, initY = e.pageY;
+            // console.log("X: " + dragX + " Y: " + dragY);
+            // console.log("X: " + deltaX + " Y: " + deltaY);
+
+            targetX += (deltaX / zoom) * 30
+            targetY += (deltaY / zoom) * 30
         }
     });
 
     window.addEventListener('mouseup', function () {
         down = false
 
-
     });
+
+    let zoomTarget = 20
+    window.addEventListener('mousewheel', function (evt) {
+        let delta = evt.wheelDelta
+        zoomTarget += delta * 0.02
+        if (zoomTarget <= 2) {
+            zoomTarget = 2
+        }
+    });
+    
+    console.log(document.getElementById('card'))
+    
     
     function main(currentTime) {
         window.requestAnimationFrame(main);
         
+        zoom = lerp(zoom, zoomTarget, 0.05)
+
         ctx.fillStyle = backgroundColor; // Background colour
         ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        finalX = lerp(cameraPos.x, targetX, 0.25) * (zoom * 0.1)
+        finalY = lerp(cameraPos.y, targetY, 0.25) * (zoom * 0.1)
+        cameraPos.x = lerp(cameraPos.x, targetX, 0.25)
+        cameraPos.y = lerp(cameraPos.y, targetY, 0.25)
+
+        console.log(document.getElementsByClassName("object").length)
         
-        finalX = lerp(cameraPos.x, targetX, 0.1) * (zoom * 0.1)
-        finalY = lerp(cameraPos.y, targetY, 0.1) * (zoom * 0.1)
-        cameraPos.x = lerp(cameraPos.x, targetX, 0.1)
-        cameraPos.y = lerp(cameraPos.y, targetY, 0.1)
-        
+        for (let i = 0; i < document.getElementsByClassName("object").length; i++) {
+            document.getElementById(`card-${i}`).style.transform = `translate(${cameraPos.x}px, ${cameraPos.y}px)`
+        }
+
+        // for (let i = 0; i < document.getElementsByClassName("object").length; i++) {
+        //     document.getElementById(`card-${i}`).style.transform = `translate(${window.innerWidth / 2 + cameraPos.x}px, ${window.innerHeight / 2 + cameraPos.y}px)`
+        // }
+
+        document.getElementById("container").style.transform = `translate(${window.innerWidth / 2}px, ${window.innerHeight / 2}px) scale(${zoom / 20})`
+
 
     }
 
