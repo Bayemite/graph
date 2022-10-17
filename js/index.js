f4497e0560607eeecc6d7e618d448b5a3ee41e5a
Technotech <technotech2121@gmail.com>
2022-10-17 21:39:04 +1100

    Push bezier system

--- js/index.js
+++ js/index.js
@@ -1,28 +1,32 @@
-const backgroundColor = '#181818';
+const backgroundColor = 'rgb(100, 150, 200)';
 
 let canvas = null;
 let ctx = null;
 
 let zoomScale = 0.001;
-let zoomTarget = 2;
-let zoom = 2;
+let zoomTarget = 1.5;
+let zoom = 1.5;
 
 let add = false; // Add card
 
 function newCard(i, x, y) {
     return `
-        <span id="card-${i}" style="left: ${x}px; top: ${y}px" class="object">
-          <p contenteditable role="textbox" class="text">
-            Test
-          </p>
+        <span id="card-${i}" style="left: ${Math.floor(x)}px; top: ${Math.floor(y)}px" class="object">
+          <p contenteditable role="textbox" class="text"></p>
         </span>
         `
 }
 
 function addCard(x, y) {
     let i = document.getElementsByClassName("object").length
-    console.log(i)
-    document.getElementById("translate").innerHTML += newCard(i, x, y)
+    // console.log(i)
+
+    // Hardcoded solution for now
+    // The textbox will always be placed with the default "Enter text" meaning its width will
+    // always be the same
+    // The width is 136, height is 79
+
+    document.getElementById("translate").innerHTML += newCard(i, x - 136/2, y - 79/2)
     card = document.getElementById(`card-${i}`)
 }
 
@@ -44,14 +48,14 @@ window.onload = function () {
 
     resize();
 
-    let cameraPos = new vector2D(0, 0)
+    let cameraPos = new vector2D(canvas.width / 2, canvas.height / 2)
     let mouseDown = false;
     let initX = 0, initY = 0;
     let dragX = 0, dragY = 0;
     let deltaX = 0, deltaY = 0;
     let finalX = 0, finalY = 0;
 
-    let targetX = 0, targetY = 0;
+    let targetX = canvas.width / 2, targetY = canvas.height / 2;
     let mouse = new vector2D(0, 0);
 
     const container = document.getElementById("container");
@@ -59,7 +63,6 @@ window.onload = function () {
     canvas.addEventListener('mousedown', function (e) {
         if (add) {
             addCard((mouse.x - cameraPos.x) / zoom, (mouse.y - cameraPos.y) / zoom)
-            console.log(e.pageX, e.pageY)
             document.getElementById('add').classList.remove('selected')
             add = false;
         } else {
@@ -89,7 +92,6 @@ window.onload = function () {
 
     canvas.addEventListener('mouseup', function () {
         mouseDown = false;
-
     });
 
     window.addEventListener('mousewheel', function (evt) {
@@ -102,24 +104,79 @@ window.onload = function () {
         zoomTarget = clamp(zoomOut, zoomTarget, zoomIn);
     });
 
-    console.log(document.getElementById('card'));
-
     function main(currentTime) {
         window.requestAnimationFrame(main);
-            
+
         let translateLerpScale = 0.9;
+
+        zoom = lerp(zoom, zoomTarget, 0.3);
+        cameraPos.x = lerp(cameraPos.x, targetX, translateLerpScale);
+        cameraPos.y = lerp(cameraPos.y, targetY, translateLerpScale);
+        container.style.transform = `translate(${cameraPos.x}px, ${cameraPos.y}px) scale(${zoom})`;
 
         // Template get position of cursor in "map space"
         // document.getElementById(`card-0`).style.left = `${(mouse.x - cameraPos.x) / zoom}px`
         // document.getElementById(`card-0`).style.top = `${(mouse.y - cameraPos.y) / zoom}px`
 
+        // Zoom needs fixing, needs to be anchored in the middle of the screen not 0, 0
+        // ctx.fillStyle = backgroundColor; // Background colour
+        // ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-        console.log(zoom)
+        // Clear canvas
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-        cameraPos.x = lerp(cameraPos.x, targetX, translateLerpScale);
-        cameraPos.y = lerp(cameraPos.y, targetY, translateLerpScale);
-        zoom = lerp(zoom, zoomTarget, 0.3);
-        container.style.transform = `translate(${cameraPos.x}px, ${cameraPos.y}px) scale(${zoom})`;
+        // Constants
+        let curveWidth = Math.floor(50 * zoom)
+        let limiter = 5; // Limiter before the line connection direction changes
+
+        // Connection lines
+        for (let i = 1; i < document.getElementsByClassName("object").length; i++) {
+            curveWidth = Math.floor(50 * zoom) // Set default
+
+            // Get element connecting to other element
+            let elem = document.getElementById(`card-${i}`)
+            let x2 = Math.floor(-elem.style.left.replace('px', '') * zoom - cameraPos.x)
+            let y2 = Math.floor(-elem.style.top.replace('px', '') * zoom - cameraPos.y)
+
+            // Get other element
+            let root = document.getElementById(`card-0`)
+            let xr = Math.floor(-root.style.left.replace('px', '') * zoom - cameraPos.x)
+            let yr = Math.floor(-root.style.top.replace('px', '') * zoom - cameraPos.y)
+
+            // Styling
+            ctx.fillStyle = "#fff"
+            ctx.strokeStyle = "rgba(200, 200, 200, 1)"
+            ctx.lineWidth = 2 * zoom
+
+            ctx.beginPath();
+            if (-xr + (root.offsetWidth * zoom) + (curveWidth / limiter) < -x2 - (curveWidth / limiter)) {
+                ctx.moveTo(-xr + (root.offsetWidth * zoom) - 1, -yr + (root.offsetHeight / 2) * zoom);
+                ctx.bezierCurveTo(-xr + (root.offsetWidth * zoom) + curveWidth, -yr + (root.offsetHeight / 2) * zoom, -x2 - curveWidth, -y2 + (elem.offsetHeight / 2) * zoom, -x2 + 1, -y2 + (elem.offsetHeight / 2) * zoom);
+                ctx.stroke();
+            } else if (-xr + (root.offsetWidth * zoom) + (curveWidth / limiter) > -x2 - (curveWidth / limiter) && (-xr + (curveWidth / limiter) < -x2 + (elem.offsetWidth * zoom) + (curveWidth / limiter))) {
+                if (yr > y2) {
+                    curveWidth = Math.floor(50 * zoom) * clamp(0.1, (yr - y2) / 500, 1)
+                    // console.log(curveWidth)
+                    ctx.moveTo(-xr + (root.offsetWidth / 2 * zoom) - 1, -yr + (root.offsetHeight) * zoom - 1);
+                    ctx.bezierCurveTo(-xr + (root.offsetWidth / 2 * zoom), -yr + (root.offsetHeight) * zoom + curveWidth,
+                        -x2 + (elem.offsetWidth / 2 * zoom), -y2 - curveWidth,
+                        -x2 + (elem.offsetWidth / 2 * zoom), -y2 + 1);
+                    ctx.stroke();
+                } else {
+                    ctx.moveTo(-xr + (root.offsetWidth / 2 * zoom) - 1, -yr + 1);
+                    ctx.bezierCurveTo(-xr + (root.offsetWidth / 2 * zoom), -yr - curveWidth,
+                        -x2 + (elem.offsetWidth / 2 * zoom), -y2 + (elem.offsetHeight * zoom) + curveWidth,
+                        -x2 + (elem.offsetWidth / 2 * zoom), -y2 + (elem.offsetHeight * zoom) - 1);
+                    ctx.stroke();
+                }
+            } else {
+                ctx.moveTo(-xr + 1, -yr + (root.offsetHeight / 2) * zoom);
+                ctx.bezierCurveTo(-xr - curveWidth, -yr + (root.offsetHeight / 2) * zoom,
+                    -x2 + (elem.offsetWidth * zoom) + curveWidth, -y2 + (elem.offsetHeight / 2) * zoom,
+                    -x2 + (elem.offsetWidth * zoom) - 1, -y2 + (elem.offsetHeight / 2) * zoom);
+                ctx.stroke();
+            }
+        }
     }
 
     main(); // Start the cycle
