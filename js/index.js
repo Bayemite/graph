75f3f0ac18c7cd97fc32a0445230ef8c8c1ff37e
Technotech <technotech2121@gmail.com>
2022-10-27 20:01:56 +1100

    Add some features

--- js/index.js
+++ js/index.js
@@ -6,8 +6,13 @@ let canvas = null;
 let ctx = null;
 
 let zoomScale = 0.001;
-let zoomTarget = 1.5;
+let zoomTarget = 1.0;
 let zoom = 1;
+
+let targetX;
+let targetY;
+let mouse;
+let cameraPos;
 
 // RGB class in functions.js
 let colorPalette = [
@@ -22,49 +27,128 @@ let colorPalette = [
 let data = [
     new util.cardObject(
         0, 0,
-        'Title', null, 0
+        '', null, 0
     ),
-    new util.cardObject(
-        200, 100,
-        'Title', null, 1
-    ),
-    new util.cardObject(
-        -200, 100,
-        'Title', null, 2
-    ),
+    // new util.cardObject(
+    //     200, 100,
+    //     'Title', 0, 1
+    // ),
+    // new util.cardObject(
+    //     -200, 100,
+    //     'Title', 0, 2
+    // ),
 ]
 
+// Add cards from data
+function loadCards() {
+    for (let i = 0; i < data.length; i++) {
+        addCard(data[i].x, data[i].y, data[i].title, true, data[i].id)
+    }
+}
+
+// Clears elements for loading new ones
+function clearMap() {
+    document.getElementById("translate").innerHTML = ""
+}
 
 let linkStart = 0;
 let linkEnd = 0;
 let linkInProgress = false;
 
-function link(i)
-{
+function link(i) {
+    // console.log('link ' + i)
     linkStart = i;
     linkInProgress = true;
 }
 
-function linkTo(i)
-{
+function linkTo(i) {
     linkEnd = i
     // Check same i
-    if (linkInProgress && linkStart != linkEnd)
-    {
+    if (linkInProgress && linkStart != linkEnd) {
         data[linkStart].connection = linkEnd;
         linkInProgress = false;
     }
 }
 
-function newCard(i, x, y, t)
-{
+function removeElem(i) {
+    data.splice(i, 1)
+    for (let index = 0; index < data.length; index++) {
+        if (data[i].connection == i) {
+            data[i].connection = null
+        }
+    }
+    // document.removeChild(document.getElementById('card-0'))
+    clearMap()
+    loadCards()
+}
+
+let moveCardI = 0
+let moveFlag = false
+function moveElem(i) {
+    let card = document.getElementById(`card-${i}`)
+    data[i].x = Math.floor((mouse.x - cameraPos.x) / zoom - card.offsetWidth / 2)
+    data[i].y = Math.floor((mouse.y - cameraPos.y) / zoom - card.offsetHeight / 2)
+    card.style.top = `${data[i].y}px`
+    card.style.left = `${data[i].x}px`
+}
+
+function newCard(i, x, y, t) {
     if (t == undefined) { t = "" };
 
     let card = document.createElement('span');
     card.id = "card-" + i;
     card.style = "left:" + Math.floor(x) + "px; top:" + Math.floor(y) + "px";
-    card.style.classList.add('object');
-    card.addEventListener('click', linkTo(i));
+    card.classList.add('object');
+    card.onclick = function () { linkTo(i) };
+
+    let p = document.createElement('p');
+    p.classList.add('text')
+    p.contentEditable = true
+    p.innerHTML = t
+    // p.id = `p-${i}`
+    p.onkeyup = function () {
+        data[i].title = p.innerHTML
+    };
+    card.appendChild(p)
+
+    let actions = document.createElement('div');
+    actions.classList.add("actions")
+
+    let linkElem = document.createElement('button');
+    linkElem.innerHTML = `
+    <span class='material-symbols-outlined'>
+        share
+    </span>
+    `
+    linkElem.classList.add("actions-button")
+    linkElem.onclick = function () { link(i) };
+    actions.appendChild(linkElem)
+
+    let remove = document.createElement('button');
+    remove.innerHTML = `
+    <span class="material-symbols-outlined">
+        delete
+    </span>
+    `
+    remove.classList.add("actions-button")
+    remove.onclick = function () { removeElem(i) };
+    actions.appendChild(remove)
+
+    let move = document.createElement('button');
+    move.innerHTML = `
+    <span class="material-symbols-outlined">
+    open_with
+    </span>
+    `
+    move.classList.add("actions-button")
+    move.onmousedown = function () {
+        moveFlag = true;
+        moveCardI = i;
+    };
+    // move.onmouseup = function () { moveFlag = false };
+    actions.appendChild(move)
+
+    card.appendChild(actions)
 
     return card;
     // `
@@ -77,19 +161,21 @@ function newCard(i, x, y, t)
 
 
 let add = false; // Add card
-function addCard(i, x, y, t)
-{
+let largest = 0;
+function addCard(x, y, t, newInstance, i) {
     // Hardcoded solution for now
     // The textbox will always be placed with the default "Enter text" meaning its width will
     // always be the same
     // The width is 136, height is 79
 
-    // let i = document.getElementsByClassName("object").length
-    document.getElementById("translate").appendChild(newCard(i, x - 136 / 2, y - 79 / 2, t));
-    let largest = 0;
-    for (let i = 0; i < data.length; i++)
-    {
-        if (data[i].id > largest) { largest = data[i].id }
+    if (newInstance) {
+        document.getElementById("translate").appendChild(newCard(i, x - 136 / 2, y - 79 / 2, t));
+    } else {
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].id >= largest) { largest = data[i].id }
+        }
+        document.getElementById("translate").appendChild(newCard(largest + 1, x - 136 / 2, y - 79 / 2, t));
+        data.push(new util.cardObject(x, y, "", null, largest + 1))
     }
     // data.push(
     //     {
@@ -102,18 +188,17 @@ function addCard(i, x, y, t)
     //     }
     // )
     // card = document.getElementById(`card-${i}`)
+
 }
 
 
 // Main loop
-window.onload = function()
-{
+window.onload = function () {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
     ctx.imageSmoothingEnabled = true;
 
-    function resize()
-    {
+    function resize() {
         // Resize the canvas to the screen
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
@@ -121,10 +206,18 @@ window.onload = function()
         // ctx.fillRect(0, 0, canvas.width, canvas.height);
     }
     window.addEventListener('resize', resize);
+    document.getElementById("add").onclick = function () {
+        add = !add;
+        if (add) {
+            document.getElementById('add').classList.add('selected')
+        } else {
+            document.getElementById('add').classList.remove('selected')
+        }
 
+    }
     resize();
 
-    let cameraPos = new util.vector2D(canvas.width / 2, canvas.height / 2);
+    cameraPos = new util.vector2D(canvas.width / 2, canvas.height / 2);
     let mouseDown = false;
 
     // Mouse delta
@@ -133,33 +226,42 @@ window.onload = function()
     let deltaX = 0, deltaY = 0;
     let finalX = 0, finalY = 0;
 
-    let targetX = (canvas.width / 2) / zoom, targetY = (canvas.height / 2) / zoom;
-    let mouse = new util.vector2D(0, 0);
+    targetX = (canvas.width / 2) / zoom, targetY = (canvas.height / 2) / zoom;
+    mouse = new util.vector2D(0, 0);
+
+    // Handle card movement
+    // Mouse events are up in card creation
+    document.addEventListener('mousemove', function (e) {
+        if (moveFlag) {
+            mouse.x = e.pageX;
+            mouse.y = e.pageY;
+            moveElem(moveCardI);
+        }
+    })
+
+    document.addEventListener('mouseup', function (e) {
+        moveFlag = false;
+    })
 
     const container = document.getElementById("container");
 
-    canvas.addEventListener('mousedown', function(e)
-    {
-        if (add)
-        {
-            addCard((mouse.x - cameraPos.x) / zoom, (mouse.y - cameraPos.y) / zoom);
+    canvas.addEventListener('mousedown', function (e) {
+        if (add) {
+            addCard(Math.floor((mouse.x - cameraPos.x) / zoom), Math.floor((mouse.y - cameraPos.y) / zoom));
             document.getElementById('add').classList.remove('selected');
             add = false;
-        } else
-        {
+        } else {
             mouseDown = true;
             initX = e.pageX, initY = e.pageY;
             dragX = e.pageX, dragY = e.pageY;
         }
     });
 
-    canvas.addEventListener('mousemove', function(e)
-    {
+    canvas.addEventListener('mousemove', function (e) {
         mouse.x = e.pageX;
         mouse.y = e.pageY;
 
-        if (mouseDown)
-        {
+        if (mouseDown) {
 
             dragX = e.pageX;
             dragY = e.pageY;
@@ -173,36 +275,96 @@ window.onload = function()
         }
     });
 
-    canvas.addEventListener('mouseup', function()
-    {
+    canvas.addEventListener('mouseup', function () {
         mouseDown = false;
     });
 
-    window.addEventListener('mousewheel', function(evt)
-    {
+    window.addEventListener('mousewheel', function (evt) {
         let delta = evt.wheelDelta;
         let zoomFactor = 0.007;
         zoomTarget += delta * zoomFactor;
 
         let zoomOut = 0.3;
         let zoomIn = 3;
-        zoomTarget = clamp(zoomOut, zoomTarget, zoomIn);
+        zoomTarget = util.clamp(zoomOut, zoomTarget, zoomIn);
     });
 
-
-    // Add cards from data
-    function loadCards()
-    {
-        for (let i = 0; i < data.length; i++)
-        {
-            addCard(data[i].id, data[i].x, data[i].y, data[i].title)
-        }
-    }
     loadCards()
 
+    const fileInput = document.getElementById('openFile');
+    fileInput.onchange = async function () {
+        // let selectedFile = fileInput.files[0];
+        let file = new FileReader();
+        file.onload = () => {
+            // console.log(file.result)
+            let fileData;
+            try {
+                fileData = JSON.parse(file.result)
+                fileData = fileData.data
+            } catch (error) {
+                alert("File format incompatible")
+                console.log("File incompatible")
+                return
+            }
+            data = []
+            for (let i = 0; i < fileData.length; i++) {
+                data.push(new util.cardObject(
+                    fileData[i].x,
+                    fileData[i].y,
+                    fileData[i].title,
+                    fileData[i].connection,
+                    fileData[i].id)
+                )
 
-    function main(currentTime)
-    {
+            }
+            clearMap()
+            loadCards()
+            console.log(data)
+
+        }
+        file.readAsText(this.files[0]);
+        fileInput.value = ''
+    }
+
+    // Function to download data to a file
+    // https://stackoverflow.com/questions/13405129/create-and-save-a-file-with-javascript
+    function download(data, filename, type) {
+        var file = new Blob([data], { type: type });
+        if (window.navigator.msSaveOrOpenBlob) // IE10+
+            window.navigator.msSaveOrOpenBlob(file, filename);
+        else { // Others
+            var a = document.createElement("a"),
+                url = URL.createObjectURL(file);
+            a.href = url;
+            a.download = filename;
+            document.body.appendChild(a);
+            a.click();
+            setTimeout(function () {
+                document.body.removeChild(a);
+                window.URL.revokeObjectURL(url);
+            }, 0);
+        }
+    }
+
+    document.getElementById('save').onclick = function () {
+        let saveData = {
+            data: []
+        }
+        for (let i = 0; i < data.length; i++) {
+            saveData.data.push(
+                {
+                    "x": data[i].x,
+                    "y": data[i].y,
+                    "title": data[i].title,
+                    "connection": data[i].connection,
+                    "id": data[i].id,
+                }
+            )
+        }
+        download(JSON.stringify(saveData), "Mindmap", ".json")
+    }
+
+    function main(currentTime) {
         window.requestAnimationFrame(main);
 
         let translateLerpScale = 0.9;
@@ -210,6 +372,7 @@ window.onload = function()
         zoom = util.lerp(zoom, zoomTarget, 0.3);
         cameraPos.x = util.lerp(cameraPos.x, targetX, translateLerpScale);
         cameraPos.y = util.lerp(cameraPos.y, targetY, translateLerpScale);
+        ctx.fillStyle = "#fff"
         container.style.transform = `translate(${cameraPos.x}px, ${cameraPos.y}px) scale(${zoom})`;
 
         // Template get position of cursor in "map space"
@@ -222,16 +385,16 @@ window.onload = function()
 
         // Clear canvas
         ctx.clearRect(0, 0, canvas.width, canvas.height)
+        // console.log(mouse.x, cameraPos.x, zoom, (mouse.x + cameraPos.x) - canvas.width / 2)
+        // ctx.fillRect((mouse.x + cameraPos.x) - canvas.width / 2 - 25, (mouse.y - cameraPos.y) + canvas.height / 2 - 25, 50, 50)
 
         // Constants
         let curveWidth = Math.floor(50 * zoom)
         let limiter = 5; // Limiter before the line connection direction changes
 
         // Connection lines
-        for (let i = 0; i < data.length; i++)
-        {
-            if (data[i].connection == null)
-            {
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].connection == null) {
                 continue
             }
 
@@ -252,46 +415,47 @@ window.onload = function()
             ctx.strokeStyle = "rgba(200, 200, 200, 1)"
             ctx.lineWidth = 2 * zoom
 
+            let triRad = 4
+
             // These are like
             // Stuff that like
             // Works and like
             // yeah
             ctx.beginPath();
-            if (-xr + (root.offsetWidth * zoom) < -x2)
-            {
-                curveWidth = Math.floor(50 * zoom) * clamp(0.1, (xr - x2) / 500, 1)
+            if (-xr + (root.offsetWidth * zoom) < -x2) {
+                curveWidth = Math.floor(150 * zoom) * util.clamp(0.1, (xr - x2) / zoom / 500, 1)
                 ctx.moveTo(-xr + (root.offsetWidth * zoom) - 2, -yr + (root.offsetHeight / 2) * zoom);
                 ctx.bezierCurveTo(-xr + (root.offsetWidth * zoom) + curveWidth, -yr + (root.offsetHeight / 2) * zoom,
                     -x2 - curveWidth, -y2 + (elem.offsetHeight / 2) * zoom,
                     -x2 + 1, -y2 + (elem.offsetHeight / 2) * zoom);
                 ctx.stroke();
-            } else if (-xr + (root.offsetWidth * zoom) + (curveWidth * zoom / limiter) > -x2 - (curveWidth * zoom / limiter) && (-xr + (curveWidth * zoom / limiter) < -x2 + (elem.offsetWidth * zoom) + (curveWidth * zoom / limiter)))
-            {
-                if (yr > y2)
-                {
-                    curveWidth = Math.floor(50 * zoom) * clamp(0.1, (yr - y2) / 500, 1)
+                new util.drawTriangle(ctx, -xr + (root.offsetWidth * zoom) - 2 + (triRad + 0.5) * zoom, -yr + (root.offsetHeight / 2) * zoom, triRad, zoom, '#fff', -90)
+            } else if (-xr + (root.offsetWidth * zoom) + (curveWidth * zoom / limiter) > -x2 - (curveWidth * zoom / limiter) && (-xr + (curveWidth * zoom / limiter) < -x2 + (elem.offsetWidth * zoom) + (curveWidth * zoom / limiter))) {
+                if (yr > y2) {
+                    curveWidth = Math.floor(150 * zoom) * util.clamp(0.1, (yr - y2) / zoom / 500, 1)
                     ctx.moveTo(-xr + (root.offsetWidth / 2 * zoom) - 1, -yr + (root.offsetHeight) * zoom - 1);
                     ctx.bezierCurveTo(-xr + (root.offsetWidth / 2 * zoom), -yr + (root.offsetHeight) * zoom + curveWidth,
                         -x2 + (elem.offsetWidth / 2 * zoom), -y2 - curveWidth,
                         -x2 + (elem.offsetWidth / 2 * zoom), -y2 + 1);
                     ctx.stroke();
-                } else
-                {
-                    curveWidth = Math.floor(50 * zoom) * clamp(0.1, (y2 - yr) / 500, 1)
+                    new util.drawTriangle(ctx, -xr + (root.offsetWidth / 2 * zoom) - 1, -yr + (root.offsetHeight) * zoom - 1 + (triRad + 0.5) * zoom, triRad, zoom, '#fff', 0)
+                } else {
+                    curveWidth = Math.floor(150 * zoom) * util.clamp(0.1, (y2 - yr) / zoom / 500, 1)
                     ctx.moveTo(-xr + (root.offsetWidth / 2 * zoom) - 1, -yr + 1);
                     ctx.bezierCurveTo(-xr + (root.offsetWidth / 2 * zoom), -yr - curveWidth,
                         -x2 + (elem.offsetWidth / 2 * zoom), -y2 + (elem.offsetHeight * zoom) + curveWidth,
                         -x2 + (elem.offsetWidth / 2 * zoom), -y2 + (elem.offsetHeight * zoom) - 1);
                     ctx.stroke();
+                    new util.drawTriangle(ctx, -xr + (root.offsetWidth / 2 * zoom) - 1, -yr + 1 - (triRad + 0.5) * zoom, triRad, zoom, '#fff', 180)
                 }
-            } else
-            {
-                curveWidth = Math.floor(50 * zoom) * clamp(0.1, (x2 - xr) / 500, 1)
+            } else {
+                curveWidth = Math.floor(150 * zoom) * util.clamp(0.1, (x2 - xr) / zoom / 500, 1)
                 ctx.moveTo(-xr + 1, -yr + (root.offsetHeight / 2) * zoom);
                 ctx.bezierCurveTo(-xr - curveWidth, -yr + (root.offsetHeight / 2) * zoom,
                     -x2 + (elem.offsetWidth * zoom) + curveWidth, -y2 + (elem.offsetHeight / 2) * zoom,
                     -x2 + (elem.offsetWidth * zoom) - 1, -y2 + (elem.offsetHeight / 2) * zoom);
                 ctx.stroke();
+                new util.drawTriangle(ctx, -xr + 1 - (triRad + 0.5) * zoom, -yr + (root.offsetHeight / 2) * zoom, triRad, zoom, '#fff', 90)
             }
         }
     }
--- js/util.js
+++ js/util.js
@@ -55,3 +55,53 @@ export function radiansToDegrees(radians) {
 export function getById(e) {
     return document.getElementById(e)
 }
+
+export function drawTriangle(ctx, x, y, radius, zoom, fill, angle) {
+    this.x = x
+    this.y = y
+
+    let top = new vector2D(0, 0)
+    let bottomL = new vector2D(0, 0)
+    let bottomR = new vector2D(0, 0)
+
+    // let rad = (radius * 0) + (radius * (zoom * 0.01) * (1 - 0))
+    let rad = radius * zoom
+    top.x = rad * Math.sin((0 + angle) * (Math.PI / 180)) + x
+    top.y = -rad * Math.cos((0 + angle) * (Math.PI / 180)) + y
+
+    bottomL.x = rad * Math.sin((120 + angle) * (Math.PI / 180)) + x
+    bottomL.y = -rad * Math.cos((120 + angle) * (Math.PI / 180)) + y
+    bottomR.x = rad * Math.sin((240 + angle) * (Math.PI / 180)) + x
+    bottomR.y = -rad * Math.cos((240 + angle) * (Math.PI / 180)) + y
+
+    this.draw = function (zoom) {
+        ctx.beginPath()
+        ctx.moveTo(top.x, top.y);
+        ctx.lineTo(bottomL.x, bottomL.y);
+        ctx.lineTo(bottomR.x, bottomR.y);
+        ctx.lineTo(top.x, top.y);
+        ctx.closePath();
+        ctx.strokeStyle = fill;
+        ctx.stroke();
+        ctx.fillStyle = fill
+        ctx.fill()
+    }
+
+    // this.update = function (x, y, zoom, angle, zoomScalar) {
+    //     // ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+    //     this.x = x
+    //     this.y = y
+    //     rad = (radius * zoomScalar) + (radius * (zoom * 0.01) * (1 - zoomScalar))
+    //     top.x = rad * Math.sin((0 + angle) * (Math.PI / 180)) + x
+    //     top.y = -rad * Math.cos((0 + angle) * (Math.PI / 180)) + y
+    //     bottomL.x = rad * Math.sin((120 + angle) * (Math.PI / 180)) + x
+    //     bottomL.y = -rad * Math.cos((120 + angle) * (Math.PI / 180)) + y
+    //     bottomR.x = rad * Math.sin((240 + angle) * (Math.PI / 180)) + x
+    //     bottomR.y = -rad * Math.cos((240 + angle) * (Math.PI / 180)) + y
+
+    //     this.draw(zoom);
+    // }
+
+
+    this.draw()
+}
