7a31d73419d0f03f25a0bd625d54aef82334b64b
Bayemite <bayemite@gmail.com>
2023-08-23 21:08:06 +1000

    use github pages href for collaboration link

--- js/util.js
+++ js/util.js
@@ -1896,7 +1896,7 @@ export class PeerManager {
         this.hostBtn.innerText = 'Stop Hosting';
 
         this.localPeer.on('open', () => {
-            this.collabTag.value = `127.0.0.1:5500/index.html?hostId=${this.localPeer.id}`;
+            this.collabTag.value = `bayemite.github.io/graph/index.html?hostId=${this.localPeer.id}`;
             this.onUpdate(msg => {
                 for (let conn of this.connections.values())
                     conn.send(msg);
